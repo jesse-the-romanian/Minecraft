cc2e3a9a6ba57f872e0398e9815fd19255df8287
jesse-the-romanian <99042968+jesse-the-romanian@users.noreply.github.com>
2022-02-11 10:41:17 -0800

    minecraft.js

---
+++ minecraft.js
@@ -0,0 +1,20 @@
+# Minecraft
+blocks.place(BEDROCK, pos(2, 64, 0))
+blocks.place(BEDROCK, pos(1, 64, 1))
+blocks.place(BEDROCK, pos(0, 64, 0))
+blocks.place(BEDROCK, pos(1, 64, -1))
+
+blocks.place(BEDROCK, pos(2, 63, 0))
+blocks.place(BEDROCK, pos(1, 63, 1))
+blocks.place(BEDROCK, pos(0, 63, 0))
+blocks.place(BEDROCK, pos(1, 63, -1))
+
+blocks.place(BEDROCK, pos(2, 65, 0))
+blocks.place(BEDROCK, pos(1, 65, 1))
+blocks.place(BEDROCK, pos(0, 65, 0))
+blocks.place(BEDROCK, pos(1, 65, -1))
+
+blocks.place(BEDROCK, pos(1, 63, 0))
+blocks.place(BEDROCK, pos(1, 66 , 0))
+
+player.teleport(pos(1, 64, 0))
