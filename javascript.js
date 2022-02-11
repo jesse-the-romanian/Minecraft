852c7c8145e9e996c52e65c059d63827e87d1369
jesse-the-romanian <99042968+jesse-the-romanian@users.noreply.github.com>
2022-02-11 09:30:20 -0800

    javascript.js

---
+++ javascript.js
@@ -0,0 +1,19 @@
+// here we have created a variable callled num
+let num;
+
+num = 24
+
+// multitple by 20
+num = num * 20;
+// divide by 40
+num = num / 40;
+// add 100
+num = num + 100;
+// minus 6753
+num = num - 6753;
+
+num = 0;
+
+if(num > 0){
+    player.say("the number is postive");
+} eles if ( num == 0 ) {}
